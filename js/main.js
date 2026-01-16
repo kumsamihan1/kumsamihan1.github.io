c31ffdb21c418e278cafee3895713755c9fba611
samihan kumthekar <kumsamihan@gmail.com>
2026-01-16 18:27:32 +0530

    Added 2-page website

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 const counter = document.querySelector('.counter');
+
 const target = +counter.getAttribute('data-target');
-
 let count = 0;
-const speed = 80;
 
-const updateCounter = () => {
-    count += Math.ceil(target / 100);
+function updateCounter() {
+    const increment = Math.ceil(target / 100);
+    count += increment;
+
     if (count < target) {
         counter.innerText = count;
-        setTimeout(updateCounter, speed);
+        setTimeout(updateCounter, 40);
     } else {
         counter.innerText = target + '+';
     }
-};
+}
 
 window.addEventListener('load', updateCounter);
